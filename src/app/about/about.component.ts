0283731ab98e0c402f5a0ac457dc048221d6197c
luiz-fmatos <luizf.matos@hotmail.com>
2018-05-15 19:43:10 -0300

    [First] Dev

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -13,8 +13,6 @@ export class AboutComponent implements OnInit {
   }
 
 
-  constructor() { }
-
   ngOnInit() {
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-}
+
 
 @NgModule({
   declarations: [
